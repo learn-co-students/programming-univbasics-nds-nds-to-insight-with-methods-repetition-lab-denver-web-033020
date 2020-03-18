3236f6d19cb2d193dab81c63118c624cf555aee9
indiana danaher <indiana.danaher@gmail.com>
2020-03-18 21:54:14 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
+require "pry"
 
 def directors_totals(source)
   result = {}
@@ -25,10 +26,42 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
+  directors = [] 
+  director_index2 = 0 
+  while director_index2 < source.size do
+    directors.push(source[director_index2][:name])
+    director_index2 += 1 
+    
+  end
+  return directors
   # Write this implementation
 end
 
 def total_gross(source)
+  directors = list_of_directors(source)
+  directors_totals = directors_totals(source)
+  running_total = 0 
+  numdirectors = 0 
+  while numdirectors < directors.length do 
+    
+    running_total += directors_totals[directors[numdirectors]]
+    numdirectors += 1
+  end
+  return running_total
+  
+  
+#   running_total = 0 
+#   numdirectors = 0 
+#   while numdirectors < list_of_directors(source).length
+#     current_director = list_of_directors(source)[numdirectors]
+    
+#       running_total += directors_totals(source)[][current_director]
+    
+     
+    
+#   numdirectors += 1 
+#   end
+# return running_total
   # Write this implementation
   #
   # Should use methods:
